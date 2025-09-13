4b983c900bf200db7335980bb4a0f6f79c4cb257
Arbuthnott <83089030+nicclift@users.noreply.github.com>
2025-09-13 10:42:22 +1000

    Wire up tab + assets; finalize __init__.py; tidy setup.py

--- octoprint_prettygcode/__init__.py
+++ octoprint_prettygcode/__init__.py
@@ -1,51 +1,62 @@
-# coding=utf-8
-from __future__ import absolute_import
-
+# -*- coding: utf-8 -*-
+from __future__ import annotations
 import octoprint.plugin
 
-class PrettyGCodePlugin(octoprint.plugin.StartupPlugin,
-                       octoprint.plugin.TemplatePlugin,
-                       octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin):
+
+class PrettyGCodePlugin(
+    octoprint.plugin.StartupPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.AssetPlugin,
+    octoprint.plugin.SettingsPlugin,
+):
     def on_after_startup(self):
-        self._logger.info("Pretty GCode.")
-    pass
+        self._logger.info("PrettyGCode: plugin started")
+
+    # Tell OctoPrint to add our tab
+    def get_template_configs(self):
+        return [
+            dict(
+                type="tab",
+                name="PrettyGCode",
+                template="prettygcode_tab.jinja2",
+                custom_bindings=False,
+            )
+        ]
+
+    # Tell OctoPrint which static assets to serve (relative to octoprint_prettygcode/static/)
     def get_assets(self):
         return dict(
-            js=["js/prettygcode.js","js/three.min.js",
-                "js/LineSegmentsGeometry.js","js/LineGeometry.js","js/OBJLoader.js",
-                "js/LineMaterial.js","js/LineSegments2.js","js/Line2.js","js/camera-controls.js","js/Lut.js","js/dat.gui.js"],
-            css=["css/prettygcode.css"]
+            js=[
+                "js/three.min.js",
+                "js/LineGeometry.js",
+                "js/LineSegmentsGeometry.js",
+                "js/LineMaterial.js",
+                "js/Line2.js",
+                "js/LineSegments2.js",
+                "js/OBJLoader.js",
+                "js/camera-controls.js",
+                "js/Lut.js",
+                "js/dat.gui.js",
+                "js/prettygcode.js",
+            ],
+            css=["css/prettygcode.css"],
         )
 
-            ##~~ Softwareupdate hook
-    def get_update_information(self):
-        return dict(
-            prettygcode=dict(
-                displayName="PrettyGCode",
-                displayVersion=self._plugin_version,
+    # Default plugin settings (user can override later)
+    def get_settings_defaults(self):
+        return dict(colorScheme="bySpeed", showAxes=True)
 
-                # version check: github repository
-                type="github_release",
-                user="Kragrathea",
-                repo="OctoPrint-PrettyGCode",
-                current=self._plugin_version,
-                # update method: pip
-                pip="https://github.com/Kragrathea/OctoPrint-PrettyGCode/archive/{target_version}.zip"
-            )
-        )
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+
+# If you want your plugin to be registered within OctoPrint under a
+# different name than what you defined in setup.py ("OctoPrint-PrettyGCode"),
+# you may define that here. Same goes for the other metadata derived
+# from setup.py – it can be overwritten via __plugin_xyz__ control
+# properties. See the documentation for details.
+
 __plugin_name__ = "PrettyGCode"
-__plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_pythoncompat__ = ">=3.8,<4"
+
 
 def __plugin_load__():
-    global __plugin_implementation__ 
+    global __plugin_implementation__
     __plugin_implementation__ = PrettyGCodePlugin()
-
-    global __plugin_hooks__
-    __plugin_hooks__ = {
-        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-    }
-
--- setup.py
+++ setup.py
@@ -1,65 +1,33 @@
-# coding=utf-8
-
-########################################################################################################################
-### Do not forget to adjust the following variables to your own plugin.
-
-# The plugin's identifier, has to be unique
-plugin_identifier = "prettygcode"
-
-# The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_%s" % plugin_identifier
-
-# The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
-# plugin module
-plugin_name = "OctoPrint-PrettyGCode"
-
-# The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "1.2.4"
-
-# The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
-# module
-plugin_description = "Pretty GCode Visualizer"
-
-# The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Kragrathea"
-
-# The plugin's author's mail address.
-plugin_author_email = "todo@example.com"
-
-# The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/Kragrathea/OctoPrint-PrettyGCode"
-
-# The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
-plugin_license = "AGPLv3"
-
-# Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
-
-# Additional package data to install for this plugin. The subfolders "templates", "static" and "translations" will
-# already be installed automatically if they exist.
-plugin_additional_data = []
-
-########################################################################################################################
-
 from setuptools import setup
 
-try:
-	import octoprint_setuptools
-except:
-	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-	      "the same python installation that OctoPrint is installed under?")
-	import sys
-	sys.exit(-1)
+plugin_identifier = "prettygcode"
+plugin_package = "octoprint_prettygcode"
+plugin_name = "OctoPrint-PrettyGCode"
+plugin_version = "1.3.0a1"
+plugin_description = "A fast G-code viewer tab for OctoPrint using Three.js"
+plugin_author = "Nic Clift"
+plugin_author_email = "your@email.com"  # TODO: use your GitHub/real email
+plugin_url = "https://github.com/nicclift/OctoPrint-PrettyGCode"
+plugin_license = "MIT"
 
-setup(**octoprint_setuptools.create_plugin_setup_parameters(
-	identifier=plugin_identifier,
-	name=plugin_name,
-	version=plugin_version,
-	description=plugin_description,
-	author=plugin_author,
-	mail=plugin_author_email,
-	url=plugin_url,
-	license=plugin_license,
-	requires=plugin_requires,
-	additional_data=plugin_additional_data
-))
+plugin_requires = ["OctoPrint>=1.9.0"]
+
+setup(
+    name=plugin_name,
+    version=plugin_version,
+    description=plugin_description,
+    author=plugin_author,
+    author_email=plugin_author_email,
+    url=plugin_url,
+    license=plugin_license,
+    packages=[plugin_package],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=plugin_requires,
+    entry_points={"octoprint.plugin": [f"{plugin_identifier} = {plugin_package}"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: OctoPrint",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
